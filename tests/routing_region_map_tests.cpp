9a9535e0093378d8ec53e1083feae0116c4cf4ec
a-micable <a-micable@users.noreply.github.com>
2026-06-26 16:51:34 +0300

    Add region map regression case

--- tests/routing_region_map_tests.cpp
+++ tests/routing_region_map_tests.cpp
@@ -10,3 +10,10 @@ AETHON_TEST(region_map_summarizes_weighted_samples) {
     AETHON_REQUIRE(summary.maximum >= 8.0);
     AETHON_REQUIRE(!component.select(5.0).empty());
 }
+AETHON_TEST(region_map_drops_old_samples_1) {
+    aethon::routing::RegionMap component("region_map-window-1");
+    component.observe({5, 1.0, 1.0, "old"});
+    component.observe({50, 4.0, 1.0, "new"});
+    component.clear_before(10);
+    AETHON_REQUIRE(component.size() == 1);
+}
